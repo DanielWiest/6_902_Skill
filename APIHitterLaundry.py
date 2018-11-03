cc80bdd2139c41e587e167dc74aa3ecb941d92d4
Randall Wiest <randallwiest@dhcp-18-30-109-80.dyn.mit.edu>
2018-11-02 22:19:11 -0400

    made laundry more accurate

--- APIHitterLaundry.py
+++ APIHitterLaundry.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 def getLaundryStatus(dormString,washer,dryer):
     
@@ -7,45 +7,31 @@ def getLaundryStatus(dormString,washer,dryer):
     
     dorms = {"baker":136484,"maseeh":1364830,'mccormik':136487,"burton-connor":1364811,"macgregor":1364815,"next-house":1364812}
     
-    baseURL = "http://classic.laundryview.com/appliance_status_ajax.php?lr="
+    baseURL = "https://www.laundryview.com/api/currentRoomData?school_desc_key=74&location="
     
     appendedURL = baseURL+str(dorms[dormString])
     
     r = requests.get(appendedURL)
-    theText = r.text
-    
-    washersIndex = theText.find("WASHERS")
-    dryersIndex = theText.find("DRYERS")
-    
-    washersString = []
-    dryersString = []
-    for char in theText[washersIndex+16:]:
-        washersString.append(char)
-        if char == 'e':
-            break
-    for char in theText[dryersIndex+15:]:
-        dryersString.append(char)
-        if char == 'e':
-            break
-    washersString = "".join(washersString)
-    dryersString = "".join(dryersString)
-    
-    resultWasher = "there are currently "+washersString+" washing machines in "+dormString
-    resultDryer = "there are currently "+dryersString+" dryers in "+dormString
-    
-    if washer and dryer :
-        outputString += resultWasher+" and " + resultDryer
-        print(resultWasher,resultDryer)
+    theJson = r.json()
+    numWashers = 0
+    numWashersAvail = 0
+    numDryers = 0
+    numDryersAvail = 0
+    for machine in theJson["objects"]:
+        if machine["type"] == "washFL":
+            numWashers += 1
+            if machine["status_toggle"] == 0:
+                numWashersAvail += 1
+        elif machine["type"] == "dblDry":
+            numDryers += 2
+            if machine["status_toggle"] == 0:
+                numDryersAvail += 1
+            if machine["status_toggle2"] == 0:
+                numDryersAvail += 1
+    if washer:
+        return "There are currently "+str(numWashersAvail)+" out of "+str(numWashers)+" washing machines available in "+dormString+"."
+    elif dryer:
+        return "There are currently "+str(numDryersAvail)+" out of "+str(numDryers)+" dryers available in "+dormString+"."
     else:
-        if dryer:
-            outputString +=resultDryer
-            print(resultDryer)
-        if washer:
-            outputString +=resultWasher
-            print(resultWasher)
-    print()
+        return "No status was queried."
     return outputString+"."
-
-#print(getLaundryStatus("baker",True,True))
-#getLaundryStatus("maseeh",True,True)
-#getLaundryStatus("mccormik",True,True)
